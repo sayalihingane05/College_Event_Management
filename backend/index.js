1334d2dc9b2101804928951096e1e64155a439c6
sayalihingane05 <sayalihingane00@gmail.com>
2026-01-26 14:48:35 +0530

    vercel.json add

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,4 @@
 
-// require("dotenv").config();
-
-// const express = require("express");
-// const cors = require("cors");
-// const databaseConnection = require("./database");
-
-// const eventRoutes = require("./routes/eventRoutes");
-// const userRoutes = require("./routes/userRoutes");
-
-
-
-// const app = express();
-
-// // DB connection
-// databaseConnection();
-
-// // Middlewares
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({extended:true}));
-
-
-
-
-
-// // Test route
-// app.get("/", (req, res) => {
-//   res.send("Server is working ✅");
-// });
-
-// // Routes
-// app.use("/event", eventRoutes);
-// app.use("/user", userRoutes);
-
-
-// // Start server
-// app.listen(5000, () => {
-//   console.log("App is listening on port 5000");
-// });
 
 
 
