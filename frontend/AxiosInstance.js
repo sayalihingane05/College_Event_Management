4578a2c0610925091e50bf921f3e7a6b6bc25c90
sayalihingane05 <sayalihingane00@gmail.com>
2026-01-26 17:10:05 +0530

    fix 10

--- frontend/AxiosInstance.js
+++ frontend/AxiosInstance.js
@@ -3,3 +3,6 @@ import axios from "axios";
 
 export const bookBaseURL=axios.create({
      baseURL: import.meta.env.VITE_API_URL, })
+bookBaseURL.post("/user/login", { email, password })
+  .then(res => console.log(res.data))
+  .catch(err => console.error(err));
