e3c4d2f35ec1d129f6665d363bcda4b07bcad70b
sayalihingane05 <sayalihingane00@gmail.com>
2026-01-29 14:14:19 +0530

    ready to production 3

--- frontend/AxiosInstance.js
+++ frontend/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL?.replace(/\/$/, ""),
+  baseURL: "https://college-event-management-backend.onrender.com",
   withCredentials: true,
 });
 
